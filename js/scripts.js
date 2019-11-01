d7de3aeb7716b45fff4b26da935c9a424603d168
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-01 10:09:16 -0700

    removed all for loops except one, will add if else statements inside of that one loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,14 @@
 // business logic
 var beepArray = [];
-// for loop for 1
-for (var i =0; i < 2; i++  ) {
-  beepArray.push(i);
-  console.log(beepArray);
-}
-
-
-// for loop for 2
-for (var i =0; i < 3; i++  ) {
-  beepArray.push(i);
-  console.log(beepArray);
-}
-
-// for loop for 3
-
-
-for (var i =0; i < 4; i++  ) {
-  beepArray.push(i);
-  console.log(beepArray);
-}
-
-
-
 // for loop for 4
 for (var i =0; i < 5; i++  ) {
-  beepArray.push(i);
-  console.log(beepArray);
+  beepArray.push(i);console.log(beepArray);
 }
+
+
+
+
+
 
 
 
