73468640fc20952f563e8d976d408ad2139d3d39
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-01 10:03:34 -0700

    add function to js that returns 0,1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@
 // for loop for 3
 var beepArray = [];
 
-for (var i =0; i < )
+for (var i =0; i < 4; i++  ) {
+  beepArray.push(i);
+  console.log(beepArray);
+}
 
 // for loop for 4
-
-var beep4 = [];
-
-for (var i =0; i < )
+//
+// var beep4 = [];
+//
+// for (var i =0; i < )
 
 
 
