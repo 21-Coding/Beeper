80bd7f1b2fa8747259f37db2cf243c8f81ed9c87
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-01 09:45:57 -0700

    add js function skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 // business logic
+// for loop for 3
+var beepArray = [];
 
+for (var i =0; i < )
 
+// for loop for 4
+
+var beep4 = [];
+
+for (var i =0; i < )
 
 
 
@@ -12,6 +20,6 @@
 $(document).ready(function() {
   $("form#form").submit(function (event){
     event.preventDefault();
-    
+
   })
 })
