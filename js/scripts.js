57c86a741245d7326646289058c31a8b9d8cce8f
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-07 20:26:44 -0800

    add new branching and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,30 @@
 // business logic
 
 function boopsBeep(input){
-  // var newArray = [];
   let beepArrays = [];
 
-  for (let i = 0; i <= input ; i++) {
-    let results = i.toString();
-  // console.log(beepArrays);
+  for (let i = 0; i <= input; i++) {
+    let result = i.toString();
+      if (result.includes(3)){
+        beepArrays.push('Im sorry dave Im afraid I cant do that');
+      } else if (result.includes(2)){
+        beepArrays.push('Boop');
+      } else if (result.includes(1)){
+        beepArrays.push('Beep');
+      } else {
+        beepArrays.push('');
+        console.log(beepArrays);
 
-    if (results.includes("1")) {
-      beepArrays.push("Boop!");
-
-
-
-      // $("#list-prepend").prepend("<li>" + "Beep!" + "</li>");
-
+      }
     }
-    else (results.includes("2"))
-      beepArrays.push("Beep!");
-
-      // $("#list-prepend").prepend("<li>" + "Boop!" + "</li>");
-    }
-
-    //   else (beepArrays.includes("3"))
-    //     beepArrays.push(newArray);
-    //     $("#list-prepend").prepend("<li>" + i + "</li>");
-
-
- // }
-    return beepArrays;
- // console.log(alert(beepArrays.toString()));
- // console.log(beepArrays);
-
 }
 
 
 
+  // $("#list-prepend").prepend("<li>" + "Beep!" + "</li>");
+// $("#list-prepend").prepend("<li>" + "Boop!" + "</li>");
 
-
-
-
+    //     $("#list-prepend").prepend("<li>" + i + "</li>");
 
 
 
